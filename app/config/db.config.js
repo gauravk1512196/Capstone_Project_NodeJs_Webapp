31b96fe548ba5cdc719ab2d814dca90ce020767f
gauravk1512196 <78565082+gauravk1512196@users.noreply.github.com>
2023-08-08 14:59:26 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -12,7 +12,7 @@ module.exports = {
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_DATABASE_NAME
+  DB: process.env.DB_NAME
 };
 
 // module.exports = {
